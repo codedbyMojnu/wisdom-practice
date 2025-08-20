00c618db97095a199bfa5ab0a5f633c87e965a3e
codedByMojnu <thisismojnu@gmail.com>
2025-08-20 09:21:51 +0600

    add firebase

--- firebaseAppInitialize.js
+++ firebaseAppInitialize.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCB7oYJh2IekCQ_U7HPprNTiMNotYePasY",
-  authDomain: "wisdompractice1.firebaseapp.com",
-  projectId: "wisdompractice1",
-  storageBucket: "wisdompractice1.firebasestorage.app",
-  messagingSenderId: "468092840571",
-  appId: "1:468092840571:web:d0b1997b603915d20150de",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
