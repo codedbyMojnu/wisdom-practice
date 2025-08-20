3ed97d0de271cedea18a620908b26258b228ada2
codedByMojnu <thisismojnu@gmail.com>
2025-08-20 13:16:58 +0600

    responsive update

--- src/components/ui/DashboardHeader.jsx
+++ src/components/ui/DashboardHeader.jsx
@@ -3,7 +3,7 @@ export default function DashboardHeader({ headerName }) {
     <header className="bg-[#F9F9EB] shadow-sm border-b px-6 py-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
-          <h1 className="text-2xl font-bold text-foreground font-headline">
+          <h1 className="text-2xl font-bold text-foreground font-headline sm:ml-12">
             {headerName}
           </h1>
         </div>
