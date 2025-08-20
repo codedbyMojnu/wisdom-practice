47ba4e4985c6b83a1ea6b62b198681d84ecf5728
codedByMojnu <thisismojnu@gmail.com>
2025-08-20 07:46:36 +0600

    update category

--- src/utils/wisdomCategory.js
+++ src/utils/wisdomCategory.js
@@ -1,14 +1,14 @@
 export const wisdomCategories = [
-    "Philosophical Wisdom",
-    "Practical Wisdom",
-    "Emotional Wisdom",
-    "Social Wisdom",
-    "Spiritual Wisdom",
-    "Cultural Wisdom",
-    "Scientific & Technical Wisdom",
-    "Financial Wisdom",
-    "Health & Wellness Wisdom",
-    "Leadership & Ethical Wisdom",
-    "Historical Wisdom",
-    "Environmental Wisdom"
+    "দার্শনিক জ্ঞান (Philosophical Wisdom)",
+    "ব্যবহারিক জ্ঞান (Practical Wisdom)",
+    "আবেগীয় জ্ঞান (Emotional Wisdom)",
+    "সামাজিক জ্ঞান (Social Wisdom)",
+    "আধ্যাত্মিক জ্ঞান (Spiritual Wisdom)",
+    "সাংস্কৃতিক জ্ঞান (Cultural Wisdom)",
+    "বৈজ্ঞানিক ও প্রযুক্তিগত জ্ঞান (Scientific & Technical Wisdom)",
+    "আর্থিক জ্ঞান (Financial Wisdom)",
+    "স্বাস্থ্য ও সুস্থতা সম্পর্কিত জ্ঞান (Health & Wellness Wisdom)",
+    "নেতৃত্ব ও নৈতিক জ্ঞান (Leadership & Ethical Wisdom)",
+    "ঐতিহাসিক জ্ঞান (Historical Wisdom)",
+    "পরিবেশগত জ্ঞান (Environmental Wisdom)"
 ];
